b8dd4aec7eed93298d7478213ddd06e868de841e
Manuel-LD <manuel.lade@hotmail.com>
2024-10-30 17:22:49 -0600

    Dos resultados

--- main.js
+++ main.js
@@ -15,3 +15,12 @@ function numDiv(n){
     }console.log(result);
 }
 numDiv(25);
+
+const result2 = [];
+function numDiv2(n2){
+    num2 = n2/2;
+    for(let i=0;i<2;i++){
+        result2.splice(i, 0, num2);
+    }console.log(result2);
+}
+numDiv2(50);
